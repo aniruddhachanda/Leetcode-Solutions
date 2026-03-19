210237b8fa74724423977b34e1d3d794fdfb9a2b
Aniruddha Chanda <87259179+aniruddhachanda@users.noreply.github.com>
2026-03-19 06:12:37 +0530

    Time: 2 ms (68.63%) | Memory: 47.4 MB (46.90%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -5,10 +5,12 @@ class Solution {
         int res=nums[0];
         for(int i=1;i<nums.length;i++){
             int num=nums[i];
+
             int temp=currmax;
             currmax=Math.max(num,Math.max(currmax*num,currmin*num));
-            currmin = Math.min(num,Math.min(temp*num,currmin*num));
-            res = Math.max(res,currmax);
+            currmin=Math.min(num,Math.min(temp*num,currmin*num));
+
+            res=Math.max(res,currmax);
         }
         return res;
     }
