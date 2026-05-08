85b56b509b4fa1917d34c9af3f61a78f54e56e87
Aniruddha Chanda <87259179+aniruddhachanda@users.noreply.github.com>
2026-05-08 21:28:11 +0530

    Time: 2 ms (98.08%) | Memory: 42.7 MB (30.01%) - LeetSync

---
+++ 728-self-dividing-numbers/self-dividing-numbers.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer>ans=new ArrayList<>();
+        for(int i=left;i<=right;i++){
+            if(isSelfDivide(i)){
+                ans.add(i);
+            }
+        }
+        return ans;
+    }
+    private boolean isSelfDivide(int num){
+        int temp=num;
+        while(temp>0){
+            int digit=temp%10;
+            if(digit==0||num%digit!=0){
+                return false;
+            }
+            temp/=10;
+        }
+        return true;
+    }
+}
