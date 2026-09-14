2acca00fc2d0f203da24e439b3ba795f683f2728
Aniruddha Chanda <87259179+aniruddhachanda@users.noreply.github.com>
2026-09-14 18:39:35 +0530

    Time: 2 ms (3.66%) | Memory: 46.2 MB (74.91%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.java
+++ 328-odd-even-linked-list/odd-even-linked-list.java
@@ -43,13 +43,4 @@ class Solution {
         }
         return head;
     }
-    private int size(ListNode head){
-        int count=0;
-        ListNode temp=head;
-        while(temp!=null){
-            count++;
-            temp=temp.next;
-        }
-        return count;
-    }
 }
